148c58bbc3398bbf0aa88e5fee218b8f83189688
Murilo <dixmurilox@gmail.com>
2024-04-07 20:24:52 -0300

    feat: component gift

---
+++ components/Gift.tsx
@@ -0,0 +1,12 @@
+import styles from "../styles/Gift.module.css";
+
+export default function Gift() {
+  return (
+    <div className={styles.gift}>
+      <div className={styles.top}></div>
+      <div className={styles.body}></div>
+      <div className={styles.tie1}></div>
+      <div className={styles.tie2}></div>
+    </div>
+  );
+}
