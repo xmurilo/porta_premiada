3c526236ee9097ff1255e9a6c59e5dc3ed81e1a5
Murilo <dixmurilox@gmail.com>
2024-04-07 22:19:53 -0300

    feat: logic to show if is selected

--- components/Door.tsx
+++ components/Door.tsx
@@ -1,11 +1,18 @@
 import styles from "../styles/Door.module.css";
 
-export default function Door() {
+type DoorProps = {
+  selected?: boolean;
+};
+
+export default function Door(props: DoorProps) {
+  const selected = props.selected ? styles.selected : "";
+
   return (
     <div className={styles.area}>
-      <div className={styles.structure}>
+      <div className={`${styles.structure} ${selected}`}>
         <div className={styles.door}>
           <div className={styles.number}>3</div>
+          <div className={styles.door__knob}></div>
         </div>
       </div>
       <div className={styles.floor}></div>
