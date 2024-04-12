111ca556c9dc597b471e346462271103986a7cfa
Murilo <dixmurilox@gmail.com>
2024-04-12 18:34:58 -0300

    feat: logic to open the door

--- components/Door.tsx
+++ components/Door.tsx
@@ -8,20 +8,29 @@ type DoorProps = {
 
 export default function Door(props: DoorProps) {
   const door = props.value; // Porta original
-  const selected = door.selected_ ? styles.selected : "";
+  const selected = door.selected_ && !door.open_ ? styles.selected : "";
 
-  const toggleSelection = (e: any) => {
+  const toggleSelection = (e: React.MouseEvent) => {
     props.onChange(door.alterSelected()); // gerando a nova porta
+  };
+
+  const openDoor = (e: React.MouseEvent) => {
+    e.stopPropagation(); // evita que ao clicar em um elemento pai abra a porta
+    props.onChange(door.openDoor());
+  };
+
+  function renderDoor() {
+    return (
+      <div className={styles.door}>
+        <div className={styles.number}>{door.number_}</div>
+        <div className={styles.door__knob} onClick={openDoor}></div>
+      </div>
+    );
   }
 
   return (
     <div className={styles.area} onClick={toggleSelection}>
-      <div className={`${styles.structure} ${selected}`}>
-        <div className={styles.door}>
-          <div className={styles.number}>{door.number_}</div>
-          <div className={styles.door__knob}></div>
-        </div>
-      </div>
+      <div className={`${styles.structure} ${selected}`}>{door.open_ ? false : renderDoor()}</div>
       <div className={styles.floor}></div>
     </div>
   );
