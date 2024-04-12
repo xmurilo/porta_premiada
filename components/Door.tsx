c23f3cae9ea020bb79e6f85d44c5ff63c4b9e55f
Murilo <dixmurilox@gmail.com>
2024-04-12 17:54:10 -0300

    feat: adding port select functionality

--- components/Door.tsx
+++ components/Door.tsx
@@ -2,15 +2,20 @@ import DoorModel from "@/model/Door";
 import styles from "../styles/Door.module.css";
 
 type DoorProps = {
-  door: DoorModel;
+  value: DoorModel;
+  onChange: (newDoor: DoorModel) => void;
 };
 
 export default function Door(props: DoorProps) {
-  const {door} = props
+  const door = props.value; // Porta original
   const selected = door.selected_ ? styles.selected : "";
 
+  const toggleSelection = (e: any) => {
+    props.onChange(door.alterSelected()); // gerando a nova porta
+  }
+
   return (
-    <div className={styles.area}>
+    <div className={styles.area} onClick={toggleSelection}>
       <div className={`${styles.structure} ${selected}`}>
         <div className={styles.door}>
           <div className={styles.number}>{door.number_}</div>
