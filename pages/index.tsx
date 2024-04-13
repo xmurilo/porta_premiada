0e582108004621b997a247bd9fd667bf106d9868
Murilo <dixmurilox@gmail.com>
2024-04-12 22:57:19 -0300

    feat: structure of form

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,25 @@
-import Gift from "../components/Gift";
-
-export default function Home() {
+import Card from "@/components/Card";
+import styles from "@/styles/Form.module.css";
+import Link from "next/link";
+export default function Form() {
   return (
-    <main>
-      <Gift />
-    </main>
+    <div className={styles.form} style={{ display: "flex" }}>
+      <div>
+        <Card bgcolor="#c0392c">
+          <h1>Monty Hall</h1>
+        </Card>
+        <Card></Card>
+      </div>
+
+      <div>
+        <Card></Card>
+
+        <Card bgcolor="#28a085">
+          <Link href={`/jogo/5/2`} className={styles.link}>
+            <h2 >Iniciar</h2>
+          </Link>
+        </Card>
+      </div>
+    </div>
   );
 }
