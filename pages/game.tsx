d6e41030336799aa73a6c3ac6d97de20ade2c2f2
Murilo <dixmurilox@gmail.com>
2024-04-12 21:08:47 -0300

    feat: creating the game route

---
+++ pages/game.tsx
@@ -0,0 +1,29 @@
+import styles from "@/styles/game.module.css";
+
+import Door from "@/components/Door";
+import { createDoors, updateDoors } from "@/functions/doors";
+import { useState } from "react";
+
+export default function Game() {
+  const [doors, setDoors] = useState(createDoors(3, 2));
+
+  function renderDoors() {
+    return doors.map(door => {
+      return (
+        <Door
+          key={door.number_}
+          value={door}
+          onChange={newDoor => setDoors(updateDoors(doors, newDoor))}
+        />
+      );
+    });
+  }
+  return (
+    <main id={styles.game}>
+      <div className={styles.doors}>{renderDoors()}</div>
+      <div className={styles.buttons}>
+        
+      </div>
+    </main>
+  );
+}
