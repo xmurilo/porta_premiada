e38f2147acab71d8b763ce9e6cfd2ba13be65d22
Murilo <dixmurilox@gmail.com>
2024-04-12 22:01:55 -0300

    refactor: renaming file name and making the file to be a dynamic route

--- pages/game/[doors]/[hasGift].tsx
+++ pages/game/[doors]/[hasGift].tsx
@@ -2,10 +2,22 @@ import styles from "@/styles/game.module.css";
 import Link from "next/link";
 import Door from "@/components/Door";
 import { createDoors, updateDoors } from "@/functions/doors";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import {useRouter} from "next/router";
+import DoorModel from "@/model/Door";
 
 export default function Game() {
-  const [doors, setDoors] = useState(createDoors(3, 2));
+  const router = useRouter()
+  const [doors, setDoors] = useState<DoorModel[]>([]);
+  useEffect(() => {
+    let doors = 0;
+    let hasGift = 0;
+    if(router.query.doors && router.query.hasGift) {
+      doors = +router.query.doors
+      hasGift = +router.query.hasGift
+    }
+    setDoors(createDoors(doors, hasGift))
+  },[router?.query])
 
   function renderDoors() {
     return doors.map(door => {
