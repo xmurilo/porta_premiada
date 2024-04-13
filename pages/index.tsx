326e68dda658fa3af5d1cf597a41f111dfa91d58
Murilo <dixmurilox@gmail.com>
2024-04-12 23:34:42 -0300

    feat: add component NumericEntry in the screen home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,38 @@
 import Card from "@/components/Card";
+import NumericEntry from "@/components/NumericEntry";
 import styles from "@/styles/Form.module.css";
 import Link from "next/link";
+import { useState } from "react";
 export default function Form() {
+  const [doorsQtt, setDoorsQtt] = useState(3);
+  const [withGift, setWithGift] = useState(1);
+
   return (
-    <div className={styles.form} style={{ display: "flex" }}>
+    <div className={styles.form}>
       <div>
         <Card bgcolor="#c0392c">
           <h1>Monty Hall</h1>
         </Card>
-        <Card></Card>
+        <Card>
+          <NumericEntry
+            text="Qtde Portas?"
+            value={doorsQtt}
+            onChange={newQuantity => setDoorsQtt(newQuantity)}
+          />
+        </Card>
       </div>
 
       <div>
-        <Card></Card>
-
+        <Card>
+          <NumericEntry
+            text="Porta com Presente?"
+            value={withGift}
+            onChange={newDoorWithGift => setWithGift(newDoorWithGift)}
+          />
+        </Card>
         <Card bgcolor="#28a085">
-          <Link href={`/jogo/5/2`} className={styles.link}>
-            <h2 >Iniciar</h2>
+          <Link href={`/game/${doorsQtt}/${withGift}`} className={styles.link}>
+            <h2>Iniciar</h2>
           </Link>
         </Card>
       </div>
